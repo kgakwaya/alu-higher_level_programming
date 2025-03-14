c8d93c0937a478014795f1bfd6eb2a9d60e96520
k.gakwaya <k.gakwaya@alustudent.com>
2025-03-14 14:19:08 -0700

    genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,12 @@
--- show tvs and staff 
-SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+-- This is a script that Import the database dump of hbtn_0d_tvshows to my MySQL server
+    SELECT 
+    tv_shows.title, 
+    tv_genres.name
+FROM 
+    tv_shows
+LEFT JOIN 
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN 
+    tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY 
+    tv_shows.title ASC, tv_genres.name ASC;
