13dea347d5c9080769f5cd11adc127ca92a10660
k.gakwaya <k.gakwaya@alustudent.com>
2025-03-23 09:45:37 +0000

    post

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+ Python script to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+
+    reqs = requests.post(url, data=value)
+    print(reqs.text)
