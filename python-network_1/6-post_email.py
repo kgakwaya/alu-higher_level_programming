e61e19160fc0cd8952da9e8da859a50f798d1b9e
k.gakwaya <k.gakwaya@alustudent.com>
2025-03-25 06:46:42 +0000

    post

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,4 +1,5 @@
- ke a url and display the values"""
+#!/usr/bin/python3
+"""take a url and display the values"""
 
 
 import requests
@@ -13,4 +14,4 @@ if __name__ == "__main__":
         "email": email
     }
     response = requests.post(url, data=context)
-    print("{}".format(response.text))
+    print("{}".format(response.text)) 
