8e00d54f06272b3097bf1ee90c4c8b03f8bbd609
k.gakwaya <k.gakwaya@alustudent.com>
2025-03-25 06:10:21 +0000

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""A script that fetches https://alu-intranet.hbtn.io/status
-with the  use of  urllib."""
-from urllib import request
+'''a Python script that fetches a url'''
 
-if __name__ == "__main__":
-    with request.urlopen(
-            "https://alu-intranet.hbtn.io/status"
-            if "https://intranet.hbtn.io/status".startswith("https")
-            else "https://intranet.hbtn.io/status") as response:
-        html = response.read()
+
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode("utf-8"))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
