a581ee3fd99a2560074905d1b527326c9dbba7a2
k.gakwaya <k.gakwaya@alustudent.com>
2025-03-25 06:28:11 +0000

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
-#!/usr/bin/python3
-'''a Python script that fetches a url'''
-
+'''A Python script that fetches a URL'''
 
 import urllib.request
 
-
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode("utf-8")))
+
