1f51daae0270d676fee6e401a0c1cd6f0c643ce5
k.gakwaya <k.gakwaya@alustudent.com>
2025-03-25 06:31:38 +0000

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
-'''A Python script that fetches a URL'''
+#!/usr/bin/python3
+"""Fetches https://alu-intranet.hbtn.io/status using urllib package."""
 
 import urllib.request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
-        content = response.read()
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
-
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
