598f1872236eda9c65401f02754776f6a669eaf3
kgakwaya <k.gakwaya@gmail.com>
2025-05-30 13:28:20 -0700

    rectangle3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,22 @@
-#!/usr/bin/node
-
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+class Rectangle {
+  constructor(w, h) {
+    if (
+      typeof w !== 'number' || w <= 0 ||
+      typeof h !== 'number' || h <= 0
+    ) {
+      // Return an empty object by not assigning properties
+      return;
     }
+
+    this.width = w;
+    this.height = h;
   }
 
-  print () {
+  print() {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-};
+}
+
+module.exports = Rectangle;
