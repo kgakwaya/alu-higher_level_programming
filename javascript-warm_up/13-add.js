08d54169d7d335aa023f94c40a8b15171f889da7
kgakwaya <k.gakwaya@gmail.com>
2025-05-31 14:58:45 -0700

    integer

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,5 +1,1 @@
-#!/usr/bin/node
-function add (a, b) {
-  return a + b;
-}
-module.exports = { add };
+
--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv[2];
+const arg = process.argv[2];
+const num = parseInt(arg, 10);
 
-const number = parseInt(args);
-
-if (!isNaN(number)) {
-  console.log(`My number: ${number}`);
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
 } else {
   console.log('Not a number');
 }
