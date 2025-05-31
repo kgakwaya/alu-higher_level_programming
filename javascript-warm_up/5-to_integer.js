e5667ce38b2ac9764be31a5dabad16de51503e7d
kgakwaya <k.gakwaya@gmail.com>
2025-05-31 13:00:40 -0700

    integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-console.log(add(3, 5));
+if (Number.isNaN(Number(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${Number(process.argv[2]).toFixed(0)}`);
+}
