6ccbea306a274f6046f0048dd4da2698161e3e3f
kgakwaya <k.gakwaya@alustudent.com>
2025-06-06 18:31:45 +0000

    read

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+fs.readFile(process.argv[2], 'utf-8', (err, data) => err ? console.log(err) : console.log(data));
